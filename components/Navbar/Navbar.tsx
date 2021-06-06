72f97f5db1548aa25019518c307bdbbfa6c95b7c
JIdayyy <julien_abbadie@hotmail.fr>
2021-06-06 17:50:53 +0200

    responsive

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,45 +1,44 @@
+import { getDisplayName } from "next/dist/next-server/lib/utils";
 import Link from "next/link";
-
-
-
-
-
 
 export default function Navbar() {
   return (
-    <div className="h-20 bg-mainColor  w-full flex items-center align-middle justify-around ">
+    <div className="h-20 bg-mainColor w-full flex items-center align-middle justify-around ">
       <div className="text-4xl  font-bold text-blue">Ja.</div>
-      <div>
-        <ul className=" flex">
-          <li className="text-white items-center align-middle justify-center  flex mx-4 text-xl">
-            <img className="w-4 h-4 mx-2" src="img/home.png" alt="" />
-            <Link href="/">
-              <button className="hover:border-blue  border-transparent border-b">
-                Accueil
-              </button>
-            </Link>
-          </li>
-          <li className="text-white items-center align-middle justify-center flex mx-4 text-xl">
-            <img className="w-4 h-4 mx-2" src="img/avatar.png" alt="" />
-            <Link href="/about">
-              <button className="hover:border-blue border-transparent border-b">
-                A Propos
-              </button>
-            </Link>
-          </li>
-          <li className="text-white items-center align-middle justify-center flex mx-4 text-xl">
-            <img className="w-4 h-4 mx-2" src="img/tv.png" alt="" />
-            <Link href="/project">
-              <button className="hover:border-blue border-transparent border-b">
-                Project
-              </button>
-            </Link>
-            <button>
-              <img className="mx-4" src="/img/cv.png" alt="" />
+
+      <button className="md:hidden outline-none transform  translate-x-20 w-11">
+        <img src="img/burger.png" alt="" />
+      </button>
+
+      <ul className="hidden md:flex ">
+        <li className="text-white items-center align-middle justify-center  flex mx-4 text-xl">
+          <img className="w-4 h-4 mx-2" src="img/home.png" alt="" />
+          <Link href="/">
+            <button className="hover:border-blue  border-transparent border-b">
+              Accueil
             </button>
-          </li>
-        </ul>
-      </div>
+          </Link>
+        </li>
+        <li className="text-white items-center align-middle justify-center flex mx-4 text-xl">
+          <img className="w-4 h-4 mx-2" src="img/avatar.png" alt="" />
+          <Link href="/about">
+            <button className="hover:border-blue border-transparent border-b">
+              A Propos
+            </button>
+          </Link>
+        </li>
+        <li className="text-white items-center align-middle justify-center flex mx-4 text-xl">
+          <img className="w-4 h-4 mx-2" src="img/tv.png" alt="" />
+          <Link href="/project">
+            <button className="hover:border-blue border-transparent border-b">
+              Project
+            </button>
+          </Link>
+          <button>
+            <img className="mx-4" src="/img/cv.png" alt="" />
+          </button>
+        </li>
+      </ul>
     </div>
   );
 }
