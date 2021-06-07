578edf4d9e7154eb83296340be12464142a6c865
JIdayyy <julien_abbadie@hotmail.fr>
2021-06-07 17:51:36 +0200

    fault

--- pages/project/[id].tsx
+++ pages/project/[id].tsx
@@ -1,38 +1,46 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-import Image from 'next/image'
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+import Image from "next/image";
 
-export default function myProject({title,content,Pictures,createdAt }:IProject) {
-    return(
-        <div className="w-full flex flex-col text-white items-center justify-center align-middle h-screen bg-perso">
-           <div className=" flex   p-8 flex-col items-center align-middle justify-center"> 
-                <Image width={500} height={300} src={Pictures[0].pictureUrl} />
-                <div className="text-2xl  my-4">{title}</div>
-                <div className="w-80 p-4 ">{content}</div>
-                <div className="text-xs  my-4">{createdAt}</div>
-            </div>
-        </div>
-          )
-};
+export default function myProject({
+  title,
+  content,
+  Pictures,
+  createdAt,
+}: IProject) {
+  return (
+    <div className="w-full flex flex-col text-white items-center justify-center align-middle h-screen bg-perso">
+      <div className=" flex   p-8 flex-col items-center align-middle justify-center">
+        <Image width={500} height={300} src={Pictures[0].pictureUrl} />
+        <div className="text-2xl  my-4">{title}</div>
+        <div className="w-80 p-4 ">{content}</div>
+        <div className="text-xs  my-4">{createdAt}</div>
+      </div>
+    </div>
+  );
+}
 
-export async function getServerSideProps(context){
-    const {id} = context.query
-    let project = await prisma.project.findUnique({
-        where: {
-          id: parseInt(id),
-        },
-        include:{
-            Pictures:  true
-            }
-        })
-        console.log(project)
-    let {title,content,createdAt,Pictures} = project
+export async function getServerSideProps(context) {
+  const { id } = context.query;
+  let project = await prisma.project.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+    include: {
+      Pictures: true,
+    },
+  });
 
-    createdAt = createdAt.toString()
-    
-    return{
-        props: {
-           title,content,Pictures,createdAt
-        }
-    }
+  let { title, content, createdAt, Pictures } = project;
+
+  createdAt = createdAt.toString();
+
+  return {
+    props: {
+      title,
+      content,
+      Pictures,
+      createdAt,
+    },
+  };
 }
